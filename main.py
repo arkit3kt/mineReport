5162df85e17d617150111899f41331e4bd3b692b
arkit3kt <exodust@protonmail.com>
2021-12-09 01:21:29 -0500

    add [GET] account/key

--- main.py
+++ main.py
@@ -10,6 +10,7 @@ class Report:
         self.ts = datetime.timezone.utc
         self.date_time = datetime.datetime.now(self.ts)
         self.output_path = "reports"
+        self.pub_key = "WALLET_ADDY"
 
     @property
     def getRewardsData(self):
@@ -19,14 +20,21 @@ class Report:
     def getPayoutData(self):
         return requests.get(f"{self.base_url}/api/payments").json()
 
+    @property
+    def getAccountData(self):
+        return requests.get(f"{self.base_url}/api/accounts/{self.pub_key}").json()
+
     def generateCSV(self, data):
         df = pd.json_normalize(data)
+        df['reward'] = df['reward'].astype('float')
+        #print(df.dtypes)
+        #df['reward'].round(decimals=18)
         print(df)
-        df.to_csv(f"{self.output_path}/report{self.date_time}.csv")
+        df.to_csv(f"{self.output_path}/report-{self.date_time}.csv")
 
 
+if __name__ == "__main__":
+    report = Report()
+    #report.generateCSV(json.loads(json.dumps(report.getPayoutData["payments"])))
+    report.generateCSV(json.loads(json.dumps(report.getAccountData["rewards"])))
 
-report = Report()
-report.generateCSV(json.loads(json.dumps(report.getPayoutData["payments"])))
-"""
-report.generateCSV(report.getPayoutData()['payments'])"""
