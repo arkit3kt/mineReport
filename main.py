0d7136310c95e3ed99b00f67d978bcd848fd3f32
arkit3kt <exodust@protonmail.com>
2021-12-11 02:41:47 -0500

    sqlite extract

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
 #!/bin/python3
 import json, requests, datetime
 import pandas as pd
-
+from sqlalchemy.ext.declarative import declarative_base
+Base = declarative_base()
+from db.db import *
 
 class Report:
 
@@ -32,9 +34,53 @@ class Report:
         print(df)
         df.to_csv(f"{self.output_path}/report-{self.date_time}.csv")
 
+    def extract(self):
+        check_rewards = session.query(Rewards).all()
 
+        print(f"Count of rewards stored = {len(check_rewards)}")
+        data = json.loads(json.dumps(self.getAccountData))
+        if len(data["rewards"]) > 0:
+            for i in data["rewards"]:
+                new_reward = Rewards(blockheight=i['blockheight'],
+                                     timestamp=i['timestamp'],
+                                     reward=i['reward'],
+                                     percent=i['percent'],
+                                     immature=i['immature'],
+                                     orphan=i['orphan'],
+                                     uncle=i['uncle'])
+                session.add(new_reward)
+            session.commit()
+                #last_four = list(map(lambda element: str(element)[1:4], alist))
+        check_rewards = session.query(Rewards).all()
+
+        print(f"Count of rewards stored = {len(check_rewards)}")
+        print("Printing each row : ")
+        for i in check_rewards:
+            print(i)
+        sum_rewards = data["sumrewards"]
+
+        if len(sum_rewards) > 0:
+
+            for i in sum_rewards:
+                print(i)
+                new_reward = RewardSums(interval=i['inverval'],
+                                     reward=i['reward'],
+                                     numreward=i['numreward'],
+                                     name=i['name'],
+                                     offset=i['offset'])
+                session.add(new_reward)
+            session.commit()
+                #last_four = list(map(lambda element: str(element)[1:4], alist))
+        check_rewards = session.query(RewardSums).all()
+
+        print(f"Count of rewards stored = {len(check_rewards)}")
+        print("Printing each row : ")
+        for i in check_rewards:
+            print(i)
 if __name__ == "__main__":
     report = Report()
     #report.generateCSV(json.loads(json.dumps(report.getPayoutData["payments"])))
-    report.generateCSV(json.loads(json.dumps(report.getAccountData["rewards"])))
+    #report.generateCSV(json.loads(json.dumps(report.getAccountData["rewards"])))
+    #data = json.loads(json.dumps(report.getAccountData["rewards"]))
+    report.extract()
 
