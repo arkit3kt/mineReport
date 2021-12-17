8bee72ffcc4b33bfaa9fec7d6184c7a29bb98231
arkit3kt <exodust@protonmail.com>
2021-12-16 22:28:42 -0500

    working report generation with groupby, sum and crypto price
    TODO : Add electrical reporting

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 #!/bin/python3
 import json, requests, datetime
 import pandas as pd
+import numpy as np
 from sqlalchemy.ext.declarative import declarative_base
 Base = declarative_base()
 from db.db import *
@@ -9,10 +10,12 @@ class Report:
 
     def __init__(self):
         self.base_url = "https://eth.2miners.com"
+        self.base_url_cc = "https://min-api.cryptocompare.com/data/v2/histoday"
         self.ts = datetime.timezone.utc
         self.date_time = datetime.datetime.now(self.ts)
         self.output_path = "reports"
-        self.pub_key = "WALLET_ADDY"
+        self.pub_key = ""
+        self.crypto_compare_key = ""
 
     @property
     def getRewardsData(self):
@@ -26,20 +29,35 @@ class Report:
     def getAccountData(self):
         return requests.get(f"{self.base_url}/api/accounts/{self.pub_key}").json()
 
+    def getPriceData(self, ticker, limit):
+        return requests.get(f"{self.base_url_cc}?fsym={ticker}&tsym=USD&limit={limit}&toTs=1639550873&api_key={self.crypto_compare_key}").json()['Data']['Data']
+
+
     def generateCSV(self, data):
         df = pd.json_normalize(data)
-        df['reward'] = df['reward'].astype('float')
-        #print(df.dtypes)
-        #df['reward'].round(decimals=18)
-        print(df)
-        df.to_csv(f"{self.output_path}/report-{self.date_time}.csv")
+        df['date'] = pd.to_datetime(df['timestamp'], unit='s').dt.date
+        grouped = df.groupby('date', as_index=False)['reward'].sum()
+        grouped['reward'] = grouped['reward'].apply(lambda x: float('.{0:0>9}'.format(x)))
+        #grouped['reward'].
+        length = len(df['timestamp'])
+
+        """floor = np.floor(df['timestamp'])
+        ceiling = np.ceil(df['timestamp'])"""
+        grouped['timestamp'] = [df['timestamp'][length-1], df['timestamp'][0]]
+
+        num_rows = len(grouped.index) - 1
+        price_data = self.getPriceData("ETH", num_rows)
+        prices = [x['close'] for x in price_data]
+        grouped['price'] = prices
+        grouped['earned'] = grouped['reward'] * grouped['price']
+        grouped.to_csv(f"{self.output_path}/report-{self.date_time}.csv")
 
     def extract(self):
         check_rewards = session.query(Rewards).all()
 
         print(f"Count of rewards stored = {len(check_rewards)}")
         data = json.loads(json.dumps(self.getAccountData))
-        if len(data["rewards"]) > 0:
+        """if len(data["rewards"]) > 0:
             for i in data["rewards"]:
                 new_reward = Rewards(blockheight=i['blockheight'],
                                      timestamp=i['timestamp'],
@@ -56,7 +74,7 @@ class Report:
         print(f"Count of rewards stored = {len(check_rewards)}")
         print("Printing each row : ")
         for i in check_rewards:
-            print(i)
+            print(i)"""
         sum_rewards = data["sumrewards"]
 
         if len(sum_rewards) > 0:
@@ -82,5 +100,6 @@ if __name__ == "__main__":
     #report.generateCSV(json.loads(json.dumps(report.getPayoutData["payments"])))
     #report.generateCSV(json.loads(json.dumps(report.getAccountData["rewards"])))
     #data = json.loads(json.dumps(report.getAccountData["rewards"]))
-    report.extract()
+    #report.extract()
+    report.generateCSV(json.loads(json.dumps(report.getAccountData["rewards"])))
 
